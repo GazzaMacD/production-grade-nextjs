aa71b58c2efc48a69c6b5fc5ef1eebca5ccbf6fe
Gareth Macdonald <macdonald.gareth@gmail.com>
2021-10-09 04:16:24 +0200

    render content from two sources using getStaticProps

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -6,10 +6,18 @@ import { useRouter } from 'next/router'
 import { Post } from '../../types'
 import Container from '../../components/container'
 import HomeNav from '../../components/homeNav'
+import path from 'path'
+import fs from 'fs'
+import matter from 'gray-matter'
+import { posts } from '../../content'
+import renderToString from 'next-mdx-remote/render-to-string'
 
 const BlogPost: FC<Post> = ({ source, frontMatter }) => {
-  const content = hydrate(source)
   const router = useRouter()
+  let content
+  if (source) {
+    content = hydrate(source)
+  }
 
   if (router.isFallback) {
     return (
@@ -42,6 +50,52 @@ const BlogPost: FC<Post> = ({ source, frontMatter }) => {
 BlogPost.defaultProps = {
   source: '',
   frontMatter: { title: 'default title', summary: 'summary', publishedOn: '' },
+}
+
+export function getStaticPaths() {
+  const postsDir = path.join(process.cwd(), 'posts')
+  const fileNames = fs.readdirSync(postsDir)
+  const slugs = fileNames.map((fileName) => {
+    const filePath = path.join(postsDir, fileName)
+    const file = fs.readFileSync(filePath, 'utf-8')
+    const { data } = matter(file)
+    return data.slug
+  })
+  return {
+    paths: slugs.map((slug) => {
+      return {
+        params: {
+          slug: slug,
+        },
+      }
+    }),
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  let post
+  try {
+    const filePath = path.join(process.cwd(), 'posts', params.slug + '.mdx')
+    const file = fs.readFileSync(filePath, 'utf-8')
+    post = matter(file)
+  } catch {
+    const cmsPosts = posts.published.map((post) => {
+      return matter(post)
+    })
+    post = cmsPosts.find((post) => {
+      return post.data.slug === params.slug
+    })
+  } // catch
+  const { data } = post
+  const mdxSource = await renderToString(post.content)
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: data,
+    },
+  }
 }
 
 /**
